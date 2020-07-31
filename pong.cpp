0fa61480773daa082a9cb9b366063c797789c6eb
Joe Harrison <josephharrison1@me.com>
2020-07-31 15:23:53 +0100

    implemented some shit but no collision detection yet

--- pong.cpp
+++ pong.cpp
@@ -1,8 +1,63 @@
-#include <SFML/Graphics.hpp>
+#include "pong.hpp"
+
+#include <array>
+
+Collidable::Collidable(const sf::Vector2f& pos, const sf::Color& colour) : pos(pos), colour(colour){};
+
+const sf::Color& Collidable::get_colour() const {
+    return colour;
+}
+
+void Collidable::set_colour(const sf::Color& col) {
+    colour = col;
+}
+
+const sf::Vector2f& Collidable::get_pos() const {
+    return pos;
+}
+
+void Collidable::set_pos(const sf::Vector2f& p) {
+    pos = p;
+}
+
+void Collidable::move(const sf::Vector2f& movement) {
+    pos += movement;
+}
+
+Paddle::Paddle(const sf::Vector2f& size, const sf::Vector2f& pos, const sf::Color& colour) : size(size), Collidable(pos, colour) {
+    shape = sf::RectangleShape(size);
+}
+
+bool Paddle::bounded(const sf::Vector2f& point) const {}
+
+void Paddle::draw(sf::RenderWindow& win) const {
+    win.draw(shape);
+}
+
+Ball::Ball(int radius, const sf::Vector2f& pos, const sf::Color& colour) : radius(radius), Collidable(pos, colour) {}
+
+bool Ball::bounded(const sf::Vector2f& point) const {}
+
+void Ball::draw(sf::RenderWindow& win) const {
+    win.draw(shape);
+}
+
+void Ball::bounce(const sf::Vector2f& PoI) {}
 
 int main() {
     sf::RenderWindow GameWindow(sf::VideoMode(1280, 720), "");
     GameWindow.setTitle("Pong");
+    GameWindow.setFramerateLimit(60);
+
+    // create the objects, store references to them in an array
+    Paddle p1(sf::Vector2f(2.0f, 2.0f), sf::Vector2f(0.0f, 0.0f), sf::Color::White);
+    Paddle p2(sf::Vector2f(2.0f, 2.0f), sf::Vector2f(0.0f, 0.0f), sf::Color::White);
+    Ball ball(5, sf::Vector2f(100.0f, 100.0f), sf::Color::Red);
+
+    std::array<Collidable*, 3> objects;
+    objects[0] = &p1;
+    objects[1] = &p2;
+    objects[2] = &ball;
 
     while(GameWindow.isOpen()) {
         sf::Event event;
@@ -14,5 +69,13 @@ int main() {
                     break;
             }
         }
+        GameWindow.clear(sf::Color::Black);
+
+        // draw that frame
+        for(auto a : objects) {
+            a->draw(GameWindow);
+        }
+
+        GameWindow.display();
     };
 }
--- pong.hpp
+++ pong.hpp
@@ -3,54 +3,52 @@
 #include <SFML/Graphics.hpp>
 class Collidable {
    protected:
-    sf::Vector2i pos;
+    sf::Vector2f pos;
     sf::Color colour;
 
    public:
-    virtual bool bounded(const sf::Vector2i& point) const;
-    virtual void draw() const;
-    friend bool collided(const Collidable& objA, const Collidable& objB);
+    Collidable(const sf::Vector2f& pos, const sf::Color& colour);
 
-    const sf::Vector2i& get_size() const;
+    virtual bool bounded(const sf::Vector2f& point) const = 0;
+    virtual void draw(sf::RenderWindow& win) const = 0;
+    friend bool collided(const Collidable& objA, const Collidable& objB);
 
     const sf::Color& get_colour() const;
     void set_colour(const sf::Color& colour);
 
-    const sf::Vector2i& get_pos() const;
-    void set_pos(const sf::Vector2i& pos);
-    void move(const sf::Vector2i& movement);
+    const sf::Vector2f& get_pos() const;
+    void set_pos(const sf::Vector2f& pos);
+    void move(const sf::Vector2f& movement);
 };
 
 bool collided(const Collidable& objA, const Collidable& objB);
 
-class Paddle : Collidable {
+class Paddle : public Collidable {
    private:
-    const sf::Vector2i size;
+    const sf::Vector2f size;
     sf::RectangleShape shape;
 
    public:
-    Paddle(sf::Vector2i size, sf::Vector2i pos, sf::Color colour);
-    Paddle(sf::Vector2i size, sf::Color colour);
+    Paddle(const sf::Vector2f& size, const sf::Vector2f& pos, const sf::Color& colour);
 
-    bool bounded(const sf::Vector2i& point) const;
+    bool bounded(const sf::Vector2f& point) const override;
 
-    void draw() const;
+    void draw(sf::RenderWindow& win) const override;
 };
 
-class Ball : Collidable {
+class Ball : public Collidable {
    private:
     const int radius;
     sf::CircleShape shape;
 
    public:
-    Ball(sf::Vector2i size, sf::Vector2i pos, sf::Color colour);
-    Ball(sf::Vector2i size, sf::Color colour);
+    Ball(int radius, const sf::Vector2f& pos, const sf::Color& colour);
 
-    bool bounded(const sf::Vector2i& point) const;
+    bool bounded(const sf::Vector2f& point) const override;
 
-    void draw() const;
+    void draw(sf::RenderWindow& win) const override;
 
-    void bounce(const sf::Vector2i& impact);
+    void bounce(const sf::Vector2f& PoI);
 };
 
 #endif
