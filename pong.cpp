766bcd1890d6a74c53ed064232db31d91f95bf43
Joe Harrison <josephharrison1@me.com>
2020-09-06 16:26:22 +0100

    fucking works lads plus tidied up a bit

--- pong.cpp
+++ pong.cpp
@@ -4,20 +4,24 @@
 #include <cmath>
 #include <iostream>
 
+// helper function to print a vector via operator overload
 std::ostream& operator<<(std::ostream& os, const sf::Vector2i& v) {
     os << '(' << v.x << ", " << v.y << ')';
     return os;
 }
-
+// helper to get magnitude for a vector, for normalising it
 inline float magnitude(const sf::Vector2f& v) {
     return sqrtf(v.x * v.x + v.y * v.y);
 }
 
-inline float dot(const sf::Vector2f& v1, const sf::Vector2f v2) {
-    return (v1.x + v2.x) * (v1.y + v2.y);
-}
+// functions for the object base class
+// constructor, sets *shape to nullptr as not to leave it uninitialised
+// but this constructor isnt meant to be called as this class is purely a base for the other objects
+// and to increase code resue bc they are largely similar
+// I am well aware this is overcomplicated for something so simple but i wanted to do it for the sake of it
 object::object() : shape(nullptr){};
 
+// colour getters and setters
 const sf::Color& object::get_colour() const {
     return shape->getFillColor();
 }
@@ -26,6 +30,7 @@ void object::set_colour(const sf::Color& col) {
     shape->setFillColor(col);
 }
 
+// position getters and setters
 const sf::Vector2f& object::get_pos() const {
     return shape->getPosition();
 }
@@ -34,48 +39,52 @@ void object::set_pos(const sf::Vector2f& pos) {
     shape->setPosition(pos);
 }
 
+// move the shape
 void object::move(const sf::Vector2f& movement) {
     shape->move(movement);
 }
 
+// draw the shape to the window
 void object::draw(sf::RenderWindow& win) const {
     win.draw(*shape);
 }
+// speed getters and setters
+void object::set_speed(float s) {
+    speed = s;
+}
 
-Paddle::Paddle(const sf::Vector2f& size, const sf::Vector2f& pos, const sf::Color& colour) : size(size) {
+float object::get_speed() const {
+    return speed;
+}
+
+// Paddle class functions
+// constructor inits attributes and passes saves a shape pointer so the base half of the class can do stuff
+Paddle::Paddle(const sf::Vector2f& size, const sf::Vector2f& pos, const sf::Color& colour, float speed) : size(size), speed(speed) {
     rect = sf::RectangleShape(size);
     rect.setPosition(pos);
     shape = &rect;
 }
 
-bool Paddle::bounded(const sf::Vector2f& pos) const {
-    return rect.getGlobalBounds().contains(pos);
-}
-
-Ball::Ball(int radius, const sf::Vector2f& pos, const sf::Color& colour) : radius(radius) {
+// Ball class functions
+// constructor does similar to rect
+Ball::Ball(int radius, const sf::Vector2f& pos, const sf::Color& colour, float speed) : radius(radius), speed(speed) {
     circle = sf::CircleShape(radius);
     circle.setPosition(pos);
+    circle.setFillColor(colour);
     shape = &circle;
-    trajectory = sf::Vector2f(0.0f, 0.0f);
+    trajectory = sf::Vector2f(0.0f, 0.0f);  // zeroed intially so that we dont forget to redo it
 }
 
-const sf::Vector2f& Ball::get_centre() const {
-    return circle.getPosition() + sf::Vector2f(circle.getRadius(), circle.getRadius());
-}
-
-bool Ball::bounded(const sf::Vector2f& point) const {
-    auto centre = get_centre();
-    return (magnitude(centre - point) < circle.getRadius());
-}
-
+// bounces the ball with respect to the normal of the surface its bouncing off
 void Ball::bounce(const sf::Vector2f& norm) {
-    if (norm.x != 0) {
-        trajectory.x *= norm.x;
+    if (norm.x == 0) {
+        trajectory.y *= -1;
     } else {
-        trajectory.y *= norm.y;
+        trajectory.x *= -1;
     }
 }
 
+// getters and setters for the trajectory of the ball
 void Ball::set_trajectory(const sf::Vector2f& v) {
     trajectory.x = v.x / magnitude(v);
     trajectory.y = v.y / magnitude(v);
@@ -85,31 +94,41 @@ const sf::Vector2f& Ball::get_trajectory() const {
     return trajectory;
 }
 
+// collision detection functions, one for a paddle and a ball, another for just a bounding rect and a ball
 bool collided(const Ball& b, const Paddle& r) {
     return b.circle.getGlobalBounds().intersects(r.rect.getGlobalBounds());
 }
 
+bool collided(const Ball& b, const sf::FloatRect& r) {
+    return b.circle.getGlobalBounds().intersects(r);
+}
+
 int main() {
+    // setup game window
     sf::RenderWindow GameWindow(sf::VideoMode(1280, 720), "");
     GameWindow.setTitle("Pong");
     GameWindow.setFramerateLimit(60);
 
-    // create the objects, store references to them in an arrayw
-
-    Paddle pL(sf::Vector2f(30.0f, 400.0f), sf::Vector2f(30.0f, 70.0f), sf::Color::White);
-    Paddle pR(sf::Vector2f(30.0f, 400.0f), sf::Vector2f(1250.0f, 70.0f), sf::Color::White);
-    Ball ball(30, sf::Vector2f(640.0f, 360.0f), sf::Color::Red);
+    // create the objects, store references to them in an array
+    Paddle pL(sf::Vector2f(30.0f, 400.0f), sf::Vector2f(30.0f, 70.0f), sf::Color::White, 5);
+    Paddle pR(sf::Vector2f(30.0f, 400.0f), sf::Vector2f(1250.0f, 70.0f), sf::Color::White, 5);
+    Ball ball(15, sf::Vector2f(640.0f, 360.0f), sf::Color::Red, 5);
     std::array<object*, 3> objects;
     objects[0] = &pL;
     objects[1] = &pR;
     objects[2] = &ball;
 
-    sf::Vector2i mcoords;
-    ball.set_trajectory(sf::Vector2f(1.0f, 1.0f));  // set initial ball speed
-    ball.speed = 2;
-    pL.speed = pR.speed = 5;
+    // create boundaries for the ball
+    sf::FloatRect top(0, -5, 1280, 5);
+    sf::FloatRect bottom(0, 720, 1280, 5);
+    sf::FloatRect left(-5, 0, 5, 720);
+    sf::FloatRect right(1280, 0, 5, 720);
 
-    pL.speed = 5;
+    // variable to store mouse coords for debug purposes
+    sf::Vector2i mcoords;
+
+    ball.set_trajectory(sf::Vector2f(1.0f, 1.0f));  // set initial ball trajectory (this should be done randomly)
+
     while (GameWindow.isOpen()) {
         sf::Event event;
 
@@ -123,32 +142,33 @@ int main() {
             }
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            pL.move(sf::Vector2f(0, -1) * pL.speed);
+            pL.move(sf::Vector2f(0, -1) * pL.get_speed());
         } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            pL.move(sf::Vector2f(0, 1) * pL.speed);
+            pL.move(sf::Vector2f(0, 1) * pL.get_speed());
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-            pR.move(sf::Vector2f(0, -1) * pR.speed);
+            pR.move(sf::Vector2f(0, -1) * pR.get_speed());
         } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-            pR.move(sf::Vector2f(0, 1) * pR.speed);
+            pR.move(sf::Vector2f(0, 1) * pR.get_speed());
         }
 
         std::cout << (mcoords = sf::Mouse::getPosition(GameWindow)) << std::endl;
 
         GameWindow.clear(sf::Color::Black);
 
-        if (collided(ball, pL)) {
+        if (collided(ball, top)) {
+            ball.bounce(sf::Vector2f(0, 1));
+        } else if (collided(ball, bottom)) {
+            ball.bounce(sf::Vector2f(0, -1));
+        } else if (collided(ball, pL)) {
             ball.bounce(sf::Vector2f(1, 0));
         } else if (collided(ball, pR)) {
             ball.bounce(sf::Vector2f(-1, 0));
-        } else if (ball.get_pos().x <= 0) {
-            ball.bounce(sf::Vector2f(0, 1));
-        } else if (ball.get_pos().x >= GameWindow.getSize().x) {
-            ball.bounce(sf::Vector2f(0, -1));
         }
 
-        ball.move(ball.get_trajectory() * ball.speed);
+        ball.move(ball.get_trajectory() * ball.get_speed());
+
         // draw that frame
         for (auto a : objects) {
             a->draw(GameWindow);
--- pong.hpp
+++ pong.hpp
@@ -3,16 +3,19 @@
 
 #include <SFML/Graphics.hpp>
 
-#define PI 3.141592653
+// forward declarations
+class Ball;
+class Paddle;
+class object;
 
+// base class for all the objects to inherit from, mainly for ease of managing them
 class object {
    protected:
     sf::Shape* shape;
+    float speed;
 
    public:
-    object();
-
-    virtual bool bounded(const sf::Vector2f& point) const = 0;
+    object();  // define a constructor thats non-default but does nothing
 
     void draw(sf::RenderWindow& win) const;
 
@@ -22,46 +25,42 @@ class object {
     const sf::Vector2f& get_pos() const;
     void set_pos(const sf::Vector2f& pos);
     void move(const sf::Vector2f& movement);
+
+    void object::set_speed(float s);
+    float object::get_speed() const;
 };
 
-class Ball;
-
+// the paddles for the players to control
 class Paddle : public object {
    private:
     const sf::Vector2f size;
     sf::RectangleShape rect;
+    float speed;
 
    public:
-    float speed;
-    Paddle(const sf::Vector2f& size, const sf::Vector2f& pos, const sf::Color& colour);
-
-    bool bounded(const sf::Vector2f& point) const override;
+    Paddle(const sf::Vector2f& size, const sf::Vector2f& pos, const sf::Color& colour, float speed);
 
     friend bool collided(const Ball& b, const Paddle& r);
 };
 
+// the ball object
 class Ball : public object {
    private:
     const int radius;
     sf::CircleShape circle;
     sf::Vector2f trajectory;
-
-   public:
     float speed;
 
-    Ball(int radius, const sf::Vector2f& pos, const sf::Color& colour);
-
-    bool bounded(const sf::Vector2f& point) const override;
-
-    const sf::Vector2f& get_centre() const;
+   public:
+    Ball(int radius, const sf::Vector2f& pos, const sf::Color& colour, float speed);
 
     void bounce(const sf::Vector2f& norm);
 
     void set_trajectory(const sf::Vector2f& v);
-
     const sf::Vector2f& get_trajectory() const;
 
     friend bool collided(const Ball& b, const Paddle& r);
+    friend bool collided(const Ball& b, const sf::FloatRect& r);
 };
 
 #endif
